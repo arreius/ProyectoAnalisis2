f2ec928616789129601722c1166049ca99199f48
Luis Arrecis <luisarrecis2@gmail.com>
2022-09-28 18:35:21 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoAnalisis2/Controllers/RecordsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoAnalisis2.Controllers
+{
+    public class RecordsController : Controller
+    {
+        // GET: Records
+
+        public ActionResult Inventory()
+        {
+            return View();
+        }
+        public ActionResult Nomenclature()
+        {
+            return View();
+        }
+        public ActionResult CostCenter()
+        {
+            return View();
+        }
+    }
+}
